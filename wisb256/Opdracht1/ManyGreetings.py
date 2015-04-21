f398585ddb45d7fe79304f3c6db35bc3ee65025d
sempf1992 <stefanfranssen1992@hotmail.com>
2015-04-21 08:43:14 +0000

    Final version

--- wisb256/Opdracht1/ManyGreetings.py
+++ wisb256/Opdracht1/ManyGreetings.py
@@ -2,7 +2,7 @@
 
 Name = raw_input('Name? ')
 try:
-   Amount = int(raw_input("How many times? "))
+   Amount = int(raw_input("How many greetings? "))
    print( str(Amount)+ " x Hello, " + Name + "!")
 except Exception, e:
    print("Error, "+Amount+ " is not a number")
