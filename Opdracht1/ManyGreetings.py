37fa9076317c3f148aa81356c060a2fd5906402e
sempf1992 <stefanfranssen1992@hotmail.com>
2015-04-23 13:45:49 +0000

    To python 3

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,8 +1,8 @@
 # This is the first hand in exercise.
 
-Name = raw_input('Name? ')
+Name = input('Name? ')
 try:
-   Amount = int(raw_input("How many greetings? "))
+   Amount = int(input("How many greetings? "))
    print( str(Amount)+ " x Hello, " + Name + "!")
 except Exception, e:
    print("Error, "+Amount+ " is not a number")
