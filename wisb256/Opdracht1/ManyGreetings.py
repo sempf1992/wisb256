dd51f3ac0be73b4ac09fc76baea5a7435c8a035d
sempf1992 <stefanfranssen1992@hotmail.com>
2015-04-23 13:30:33 +0000

    Removed old files

--- wisb256/Opdracht1/ManyGreetings.py
+++
@@ -1,8 +0,0 @@
-# This is the first hand in exercise.
-
-Name = raw_input('Name? ')
-try:
-   Amount = int(raw_input("How many greetings? "))
-   print( str(Amount)+ " x Hello, " + Name + "!")
-except Exception, e:
-   print("Error, "+Amount+ " is not a number")
