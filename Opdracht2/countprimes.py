cddabfb4b9af1d64572e6827dd5fa92b604a7fcf
sempf1992 <stefanfranssen1992@hotmail.com>
2015-04-30 13:50:31 +0000

    update to last version

--- Opdracht2/countprimes.py
+++ Opdracht2/countprimes.py
@@ -6,26 +6,21 @@ C = 0.6601618
 prev = -1
 count1 = 0
 count2 = 0
-#try:
-if True:
-    with open(sys.argv[1], "r") as f:
-        for line in f:
-            #add count for a prime
-            count1 +=1
+with open(sys.argv[1], "r") as f:
+    for line in f:
+        #add count for a prime
+        count1 +=1
             
-            #see if it is a twin prime
-            if (int(line) -2 == prev):
-                count2+=1
-            prev = int(line)
-    
-    print("Largest Prime =  " + str(prev))
-    print("--------------------------------")
-    print("pi(N)         =  " + str(count1))
-    print("N/log(N)      =  " + str(prev/math.log(prev)))
-    print("ratio         =  " + str(count1 * math.log(prev)/prev))
-    print("--------------------------------")
-    print("pi2(N)        =  " + str(count2))
-    print("2CN/log(N)^2  =  " + str(2* C * prev /math.log(prev)**2))
-    print("ratio         =  " + str(count2/(2* C * prev /math.log(prev)**2)))
-#except:
-#    print("Error reading file")
+        #see if it is a twin prime
+        if (int(line) -2 == prev):
+            count2+=1
+        prev = int(line)
+print("Largest Prime =  " + str(prev))
+print("--------------------------------")
+print("pi(N)         =  " + str(count1))
+print("N/log(N)      =  " + str(prev/math.log(prev)))
+print("ratio         =  " + str(count1 * math.log(prev)/prev))
+print("--------------------------------")
+print("pi2(N)        =  " + str(count2))
+print("2CN/log(N)^2  =  " + str(2* C * prev /math.log(prev)**2))
+print("ratio         =  " + str(count2/(2* C * prev /math.log(prev)**2)))
